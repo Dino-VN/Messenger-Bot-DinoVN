b3247195bf18b82c3d8d4f3d2360917fc2021069
Dino VN <huycoibd@gmail.com>
2024-01-01 13:40:06 +0700

    Fix

--- src/core/index.ts
+++ src/core/index.ts
@@ -94,11 +94,9 @@ function startBot() {
         }
       }
 
-      //refesh appstate
-      fs.writeFileSync("./appstate.json", JSON.stringify(api.getAppState()));
-
       api.setOptions({
         listenEvents: true,
+        autoMarkDelivery: false,
         // userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
         updatePresence: true,
         autoReconnect: true,
