6f28656f598550059f8cdde64ea140d501f57d0f
Dino-VN <huycoibd@gmail.com>
2024-02-18 12:30:40 +0000

    Fix

--- src/core/functions/GlobalBan.ts
+++ src/core/functions/GlobalBan.ts
@@ -26,58 +26,62 @@ const colors = {
 const API = "https://bot.d1n0saur.xyz/api/ban_list";
 let PREFIX = `${colors.red}[Global Ban]${colors.reset}`;
 
+function GlobalBan(api: any) {
+  if (!api.config.GLOBAL_BAN) {
+    const public_ban = await users.find({ public_ban: true })
+    public_ban.forEach(async (uid) => {
+      await users.findByIdAndUpdate(uid._id, { public_ban: false, PBanReason: "" })
+      console.info(`${PREFIX} ${uid._id} đã được đưa ra khỏi danh sách ban`)
+    });
+  }
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    // Use the data retrieved from the API here
+    data.forEach(async (i: any) => {
+      let uid = i.uid
+      const reason = i.reason
+
+      const user = await users.findById(uid)
+      const public_ban = await users.find({ public_ban: true })
+
+      const public_ban_ids = public_ban!.map((u: any) => u.uid);
+      const users_not_in_data = public_ban_ids.filter((uid: string) => !data.find((user: any) => user.uid === uid));
+
+      users_not_in_data.forEach(async (uid: string) => {
+        await users.findByIdAndUpdate(uid, { public_ban: false, PBanReason: "" })
+        console.info(`${PREFIX} ${uid} đã được đưa ra khỏi danh sách ban`)
+      });
+
+      if (!user) {
+        const newUser = new users({
+          _id: uid,
+          public_ban: true,
+          PBanReason: reason
+        })
+        await newUser.save()
+        console.info(`${PREFIX} ${uid} banned với lý do: ${reason}`)
+      } else {
+        if (user.public_ban) return
+        await users.findByIdAndUpdate(uid, {
+          public_ban: true,
+          PBanReason: reason
+        })
+        console.info(`${PREFIX} ${uid} banned với lý do: ${reason}`)
+      }
+    });
+
+    setInterval(() => {
+      GlobalBan(api)
+    }, 30 * 60 * 1000)
+  } catch (e) {
+    // console.log(e)
+  }
+}
+
 export const functionFile: Function = {
   async execute(api) {
-    console.log(`${PREFIX} Đang được bật...`)
-    if (!api.config.GLOBAL_BAN) {
-      const public_ban = await users.find({ public_ban: true })
-      public_ban.forEach(async (uid) => {
-        await users.findByIdAndUpdate(uid._id, { public_ban: false, PBanReason: "" })
-        console.info(`${PREFIX} ${uid._id} đã được đưa ra khỏi danh sách ban`)
-      });
-    }
-    try {
-      const response = await fetch(API);
-      const data = await response.json();
-      // Use the data retrieved from the API here
-      data.forEach(async (i: any) => {
-        let uid = i.uid
-        const reason = i.reason
-  
-        const user = await users.findById(uid)
-        const public_ban = await users.find({ public_ban: true })
-
-        const public_ban_ids = public_ban!.map((u: any) => u.uid);
-        const users_not_in_data = public_ban_ids.filter((uid: string) => !data.find((user: any) => user.uid === uid));
-
-        users_not_in_data.forEach(async (uid: string) => {
-          await users.findByIdAndUpdate(uid, { public_ban: false, PBanReason: "" })
-          console.info(`${PREFIX} ${uid} đã được đưa ra khỏi danh sách ban`)
-        });
-
-        if (!user) {
-          const newUser = new users({
-            _id: uid,
-            public_ban: true,
-            PBanReason: reason
-          })
-          await newUser.save()
-          console.info(`${PREFIX} ${uid} banned với lý do: ${reason}`)
-        } else {
-          if (user.public_ban) return
-          await users.findByIdAndUpdate(uid, {
-            public_ban: true,
-            PBanReason: reason
-          })
-          console.info(`${PREFIX} ${uid} banned với lý do: ${reason}`)
-        }
-      });
-
-      setInterval(() => {
-        functionFile.execute(api)
-      }, 30 * 60 * 1000)
-    } catch (e) {
-      // console.log(e)
-    }
+    console.log(`${PREFIX} Đang ${api.config.GLOBAL_BAN ? "được bật": "tắt"}.`)
+    GlobalBan(api)
   },
 };
