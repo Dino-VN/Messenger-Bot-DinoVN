e47bbb7d38bfd53987288c8cda3c113f5a3f44f6
Dino-VN <huycoibd@gmail.com>
2024-02-23 06:25:23 +0000

    Fix

--- src/core/apiModule/friend.ts
+++ src/core/apiModule/friend.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { api } from "../interfaces/Map";
 import { getFb_dtsg } from "./tool";
 
@@ -5,17 +6,30 @@ export async function getRecommendedFriends(
   api: api,
   callback?: (error: any, data?: any) => void
 ) {
-  let form = {
-    av: api.getCurrentUserID(),
-    __user: api.getCurrentUserID(),
-    fb_dtsg: await getFb_dtsg(),
-    fb_api_caller_class: "RelayModern",
-    fb_api_req_friendly_name: "FriendingCometSuggestionsRootQuery",
-    variables: '{"scale":1}',
-    doc_id: "24773035982311104",
+  var options = {
+    method: 'POST',
+    url: 'https://www.facebook.com/api/graphql',
+    headers: {
+      cookie: 'c_user=61556257198316; xs=32%253AxEiMiQv-ZcfKbg%253A2%253A1708345308%253A-1%253A10601%253A%253AAcVhRFlUQD7JavBcIlSRZ1pJ-j37NzxfSer_YrVw9Xc; fr=1NCBJveR9rD13raHP.AWW9W3ZP57MfrnGqkfz8Z3p00Fo.Bl2DmR.qD.AAA.0.0.Bl2DmR.AWUf04ZqV7Q; sb=4znYZUHk6nevu0BKM9iL3Qf7; ps_n=0; ps_l=0',
+      'content-type': 'application/x-www-form-urlencoded',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+    },
+    data: {
+      av: '61556257198316',
+      __user: '61556257198316',
+      fb_dtsg: 'NAcMp3e1gvU4r5qHkBjVXqGDiRcUw1hY8-oCXVnzSSAisXQw_kvRUfw:32:1708345308',
+      fb_api_caller_class: 'RelayModern',
+      fb_api_req_friendly_name: 'FriendingCometSuggestionsRootQuery',
+      variables: '{"scale":1}',
+      doc_id: '7176500739131881'
+    }
   };
+  
+  const res = JSON.parse(await axios.request(options))
 
-  const res = JSON.parse(await api.httpPost("https://www.facebook.com/api/graphql/", form));
+  // await api.httpPost("https://www.facebook.com/api/graphql/", form));
   if (res.data) {
     if (callback) callback(null, res.data);
   } else {
